0a586d0394c5a169ce1e9fd812d179f387031494
joaopedroalb <joaopedroalb.dev@gmail.com>
2024-02-02 12:52:51 -0300

    fix: try again timeout problem in vercel

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
 import Game from "@/components/Game";
 import { getAllAnime, getDailyAnime } from "@/services/AnimeService";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default async function Home() {
   const animeData = await getDailyAnime()
   const searchOptions = await getAllAnime()
   return (
     <Box sx={{display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center', flex: 1}}>
-      <Game type='standard' animeData={animeData} searchOptions={searchOptions}/>
+      {
+        animeData ? 
+          <Game type='standard' animeData={animeData} searchOptions={searchOptions}/> : 
+          <Typography fontSize={28}>Our server is temporarily down, please try again in a few minutes... </Typography>
+      }
     </Box>
   )
 }
